5812a5b081512cbaf87405f006fdeb17c5b8989d
edinaberkes <50194278+edinaberkes@users.noreply.github.com>
2020-01-13 15:38:01 +0200

    Add files via upload

---
+++ OpenCommand.java
@@ -0,0 +1,17 @@
+
+public class OpenCommand implements Command{
+	public String st;
+	public OpenCommand(String st) {
+	this.st=st;
+	}
+
+	public void execute() {
+	    JSONBuilder jsonBuilder = new JSONBuilder("book.json");
+	    jsonBuilder.Build();
+	    Carte book = new Carte("carte1");
+	    book.addContent(jsonBuilder.GetResult());
+	    DocumentManager.getInstance().setBook(book);
+	    book.print();
+	  }
+
+}
